328fc8fe1a79fc20e7000ce970708bb6de1f4000
frmc3658 <frmc3658@colorado.edu>
2023-11-04 12:32:12 -0600

    Fixed error with SA packet counting from src instead of dst

--- detector.py
+++ detector.py
@@ -2,45 +2,48 @@ from scapy.all import PcapReader
 from scapy.all import TCP, IP, Ether, Raw
 import sys
 
+
 def process_pcap(pcap_fname):
-    # Dictionary to keep track of SYN and SYN+ACK packets count for each source IP
-    synCount = {}
-    synAckCount = {}
+    # Dictionaries for IP tracking
+    synDict = {}
+    synAckDict = {}
 
     for packet in PcapReader(pcap_fname):
-        if Ether in packet and IP in packet and TCP in packet:
-
-            # Check for SYN+ACK
-            if (packet[TCP].flags & 0x02) and (packet[TCP].flags & 0x10):
-                srcIP = packet[IP].src
-
-                # Increment SYN+ACK count for IP
-                if srcIP in synAckCount:
-                    synAckCount[srcIP] += 1
-                else:
-                    synAckCount[srcIP] = 1
-            # Otherwise check for just SYN
-            elif packet[TCP].flags & 0x02:
+        if packet.haslayer(TCP) and packet.haslayer(IP):
+            # Check for just SYN
+            if packet[TCP].flags == 'S':
                 srcIP = packet[IP].src
 
                 # Increment SYN count for IP
-                if srcIP in synCount:
-                    synCount[srcIP] += 1
+                if srcIP in synDict:
+                    synDict[srcIP] += 1
                 else:
-                    synCount[srcIP] = 1
-                    synAckCount[srcIP] = 1
+                    synDict[srcIP] = 1
+                    synAckDict[srcIP] = 1
+            # Otherwise check for SYN+ACK
+            elif packet[TCP].flags == 'SA':
+                srcIP = packet[IP].dst
+
+                # Increment SYN+ACK count for IP
+                if srcIP in synAckDict:
+                    synAckDict[srcIP] += 1
+                else:
+                    synAckDict[srcIP] = 1
 
     # Detect possible SYN scans
     suspiciousIPs = []
 
-    for srcIP, synPackets in synCount.items():
-        if srcIP in synAckCount:
-            synAckPackets = synAckCount[srcIP]
+    for srcIP, synPackets in synDict.items():
+        # If the sourvce IP is in the SYN+ACK dictionary...
+        if srcIP in synAckDict:
+            synAckPackets = synAckDict[srcIP]
 
             if synPackets > (3 * synAckPackets):
                 suspiciousIPs.append(srcIP)
 
     # Print the suspicious IP addresses
+    suspiciousIPs.sort()
+
     for ip in suspiciousIPs:
         print(ip)
 
